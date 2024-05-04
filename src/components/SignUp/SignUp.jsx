4f6f023cdc05c7617aec28a87d9e2d893a716982
okaycan <nglirongg@gmail.com>
2024-05-04 16:47:10 +0800

    Fix Sign up code

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,16 +1,13 @@
-import './SignUp.css';
 import { useState } from "react"
 // import createUser from '../utilities/user-services'
 import { signUp } from "../../utilities/users-api"
 
-function SignUp(props) {
+function SignUp() {
     const [state, setState] = useState({
         name: '',
         email: '',
         password: '',
     })
-
-    const {getUser} = props
     
     function handleChange(evt) {
         setState({...state, [evt.target.name]: evt.target.value})
@@ -18,15 +15,17 @@ function SignUp(props) {
 
     async function handleSubmit(e) {
         e.preventDefault()
-     
-        // try {
-        //     await signUp(state);
-        // } catch (error) {
-        //     console.log(error)
-        //     alert("An error occurred. Too bad.");
-        // }
-
-        getUser(state)
+        //instead of calling the console.log, this function sends the form details to 
+        //utilities, to first check if user exists, if yes, return error, if no create new user
+        //can consider doing the disableSubmit() function if any fields are empty (if we are bored)
+        //added async here becos we nid to add try catch block to interact with backend later
+        try {
+            await signUp(state);
+            alert("sign up successful")
+        } catch (error) {
+            console.log(error)
+            alert("An error occurred. Too bad.");
+        }
     }
 
     return(
