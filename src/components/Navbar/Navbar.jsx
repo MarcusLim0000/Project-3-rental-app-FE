facfc9950008ee5b3660c713b39cbdd3b4b1df98
okaycan <nglirongg@gmail.com>
2024-05-07 22:23:56 +0800

    css for nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,17 @@ import {Link} from 'react-router-dom'
 
 function Navbar({user, setUser}) {
     return(
-        <nav style={{display:'inline-flex', position:'fixed', top:'0', right:'0'}}>
-        <div style={{margin:'20px'}}><Link to='/'>Listings</Link></div>
-        <div style={{margin:'20px'}}><Link to='/profile'>My Account</Link></div>
-        <SignOut user={user} setUser={setUser}/>
-        {/* <div style={{margin:'20px'}}><Link to='/sign-out'>Sign Out</Link></div> */}
-        {/* sign out should do something to 'setUser' to null */}
-        </nav>
-    )
-}
-
+        <nav className="navbar">
+        <div className="navbar-item">
+          <Link to='/'>Listings</Link>
+        </div>
+        <div className="navbar-item">
+          <Link to='/profile'>My Account</Link>
+        </div>
+        <div className="navbar-item">
+          <SignOut user={user} setUser={setUser} />
+        </div>
+      </nav>
+    );
+  }
 export default Navbar
